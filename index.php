a4af957a8e59c2c9691f4ab1f85b712161b86b6f
System Administrator <shubham.mudgal@colorado.edu>
2016-04-19 15:30:58 -0600

    first run

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if($stmt = mysqli_prepare($db, "select itemid, itemname,price,picture,descriptio
             $price= htmlspecialchars($price);
 			$picture = htmlspecialchars($picture);
 			$desc = htmlspecialchars($desc);
+			$sellerid = htmlspecialchars($sellerid);
 			echo "<div style=\"border: 5px ridge silver;\"><table cellpadding=\"20\">
 					<tr><td> <img src=\"".$picture."\" height=\"100\" width=\"100\"></td>
                 		<td><table cellpadding=\"10\"><tr><td>Product Name</td>
@@ -39,8 +40,34 @@ if($stmt = mysqli_prepare($db, "select itemid, itemname,price,picture,descriptio
                 				<td>".$desc."</td></tr> ";
                 				
                 			if(isset($_SESSION['authenticated']) && $_SESSION['authenticated']=="yes")
-                					echo getSellerInfo($sellerid);
-                					
+                			{
+                			//		echo getSellerInfo($sellerid);
+                			
+                					$output = '';
+									if($stmt = mysqli_prepare($db, "select username,email,phone from users where userid=?"))
+									{
+									mysqli_stmt_bind_param($stmt, "s", $sellerid);
+									mysqli_stmt_execute($stmt);
+							        mysqli_stmt_bind_result($stmt, $sellername, $selleremail, $sellerphone);
+							        while(mysqli_stmt_fetch($stmt))
+									{
+										$sellername= htmlspecialchars($sellername);
+										$selleremail= htmlspecialchars($selleremail);
+							            $sellerphone= htmlspecialchars($sellerphone);
+        							}
+							        mysqli_stmt_close($stmt);
+        
+       							 $output =  $output . '<td><table cellpadding=\"10\"><tr><td>Seller\'s Name</td>
+                						<td>'.$sellername.'</td></tr>
+                						<tr><td>Email</td>
+		                				<td>$'.$selleremail.'</td></tr>
+        		        				<tr><td>Contact Number</td>
+                						<td>'.$sellerphone.'</td></tr></table></td>';
+        
+    								}
+                				
+                			
+                			}		
                 			else if(!isset($_SESSION['authenticated']) && $s==null)
                 				echo "<tr><td><a href=index.php?s=1>View Seller's Info</a></td></tr>";
                 			
